ccc6b53dd7ce422524f448529d2cc9d21b013823
Ben Parrish <finstreschwarze@gmail.com>
2023-01-16 23:52:34 -0500

    MORE BUTTONS
    passives passives passives

--- Scripts/PassiveController.gd
+++ Scripts/PassiveController.gd
@@ -27,6 +27,12 @@ onready var earth_label_1 = get_node("../TabContainer/Earth/Label1")
 onready var earth_button_2 = get_node("../TabContainer/Earth/Passive2")
 onready var earth_timer_2 = get_node("../TabContainer/Earth/Passive2/Timer")
 onready var earth_label_2 = get_node("../TabContainer/Earth/Label2")
+onready var earth_button_3 = get_node("../TabContainer/Earth/Passive3")
+onready var earth_timer_3 = get_node("../TabContainer/Earth/Passive3/Timer")
+onready var earth_label_3 = get_node("../TabContainer/Earth/Label3")
+onready var earth_button_4 = get_node("../TabContainer/Earth/Passive4")
+onready var earth_timer_4 = get_node("../TabContainer/Earth/Passive4/Timer")
+onready var earth_label_4 = get_node("../TabContainer/Earth/Label4")
 
 onready var fire_button_1 = get_node("../TabContainer/Fire/Passive1")
 onready var fire_timer_1 = get_node("../TabContainer/Fire/Passive1/Timer")
@@ -34,6 +40,12 @@ onready var fire_label_1 = get_node("../TabContainer/Fire/Label1")
 onready var fire_button_2 = get_node("../TabContainer/Fire/Passive2")
 onready var fire_timer_2 = get_node("../TabContainer/Fire/Passive2/Timer")
 onready var fire_label_2 = get_node("../TabContainer/Fire/Label2")
+onready var fire_button_3 = get_node("../TabContainer/Fire/Passive3")
+onready var fire_timer_3 = get_node("../TabContainer/Fire/Passive3/Timer")
+onready var fire_label_3 = get_node("../TabContainer/Fire/Label3")
+onready var fire_button_4 = get_node("../TabContainer/Fire/Passive4")
+onready var fire_timer_4 = get_node("../TabContainer/Fire/Passive4/Timer")
+onready var fire_label_4 = get_node("../TabContainer/Fire/Label4")
 
 onready var water_button_1 = get_node("../TabContainer/Water/Passive1")
 onready var water_timer_1 = get_node("../TabContainer/Water/Passive1/Timer")
@@ -41,6 +53,12 @@ onready var water_label_1 = get_node("../TabContainer/Water/Label1")
 onready var water_button_2 = get_node("../TabContainer/Water/Passive2")
 onready var water_timer_2 = get_node("../TabContainer/Water/Passive2/Timer")
 onready var water_label_2 = get_node("../TabContainer/Water/Label2")
+onready var water_button_3 = get_node("../TabContainer/Water/Passive3")
+onready var water_timer_3 = get_node("../TabContainer/Water/Passive3/Timer")
+onready var water_label_3 = get_node("../TabContainer/Water/Label3")
+onready var water_button_4 = get_node("../TabContainer/Water/Passive4")
+onready var water_timer_4 = get_node("../TabContainer/Water/Passive4/Timer")
+onready var water_label_4 = get_node("../TabContainer/Water/Label4")
 
 # Passive income objects and the arrays in which they are contained are declared here
 
@@ -55,12 +73,18 @@ var air_passive_4 :ResourceCreator
 
 var earth_passive_1: ResourceCreator
 var earth_passive_2: ResourceCreator
+var earth_passive_3: ResourceCreator
+var earth_passive_4: ResourceCreator
 
 var fire_passive_1: ResourceCreator
 var fire_passive_2: ResourceCreator
+var fire_passive_3: ResourceCreator
+var fire_passive_4: ResourceCreator
 
 var water_passive_1: ResourceCreator
 var water_passive_2: ResourceCreator
+var water_passive_3: ResourceCreator
+var water_passive_4: ResourceCreator
 
 var cost_label = " (Cost: %s)"
 
@@ -81,31 +105,46 @@ func _ready():
 	air_passive_2 = resource_creator.new(50, 1, 1, air_button_2, air_timer_2, air_label_2, "Construct Puff Condenser", list.RES.AIR)
 	air_passive_3 = resource_creator.new(500, 100, 10, air_button_3, air_timer_3, air_label_3, "Hire Wing Gremlins", list.RES.SUBAIR)
 	air_passive_4 = resource_creator.new(1000, 10, 1, air_button_4, air_timer_4, air_label_4, "Construct Puff Zapper", list.RES.AIR)
-	
 	all_passives.append(air_passive_1)
 	all_passives.append(air_passive_2)
 	all_passives.append(air_passive_3)
 	all_passives.append(air_passive_4)
 	
-	earth_passive_1 = resource_creator.new(25, 10, 10, earth_button_1, earth_timer_1, earth_label_1, "Hire Crag Gremlins", list.RES.SUBEARTH)
+	earth_passive_1 = resource_creator.new(25, 10, 10, earth_button_1, earth_timer_1, earth_label_1, "Hire Mining Gremlins", list.RES.SUBEARTH)
 	earth_passive_2 = resource_creator.new(50, 1, 1, earth_button_2, earth_timer_2, earth_label_2, "Construct Chunk Breaker", list.RES.EARTH)
+	earth_passive_3 = resource_creator.new(500, 100, 10, earth_button_3, earth_timer_3, earth_label_3, "Hire Crag Gremlins", list.RES.SUBEARTH)
+	earth_passive_4 = resource_creator.new(1000, 10, 1, earth_button_4, earth_timer_4, earth_label_4, "Construct Chunk Grinder", list.RES.EARTH)
 	all_passives.append(earth_passive_1)
 	all_passives.append(earth_passive_2)
+	all_passives.append(earth_passive_3)
+	all_passives.append(earth_passive_4)
 	
-	fire_passive_1 = resource_creator.new(25, 10, 10, fire_button_1, fire_timer_1, fire_label_1,  "Hire Fireproof Gremlins", list.RES.SUBFIRE)
+	fire_passive_1 = resource_creator.new(25, 10, 10, fire_button_1, fire_timer_1, fire_label_1,  "Hire Gathering Gremlins", list.RES.SUBFIRE)
 	fire_passive_2 = resource_creator.new(50, 1, 1, fire_button_2, fire_timer_2, fire_label_2, "Construct Rose Mixer", list.RES.FIRE)
+	fire_passive_3 = resource_creator.new(500, 100, 10, fire_button_3, fire_timer_3, fire_label_3,  "Hire Fireproof Gremlins", list.RES.SUBFIRE)
+	fire_passive_4 = resource_creator.new(1000, 10, 1, fire_button_4, fire_timer_4, fire_label_4, "Construct Rose Centrifuge", list.RES.FIRE)
 	all_passives.append(fire_passive_1)
 	all_passives.append(fire_passive_2)
+	all_passives.append(fire_passive_3)
+	all_passives.append(fire_passive_4)
 	
-	water_passive_1 = resource_creator.new(25, 10, 10, water_button_1, water_timer_1, water_label_1, "Hire Amphibious Gremlins", list.RES.SUBWATER)
+	water_passive_1 = resource_creator.new(25, 10, 10, water_button_1, water_timer_1, water_label_1, "Hire Snorkeling Gremlins", list.RES.SUBWATER)
 	water_passive_2 = resource_creator.new(50, 1, 1, water_button_2, water_timer_2, water_label_2, "Construct Crystal Crushers", list.RES.WATER)
+	water_passive_3 = resource_creator.new(500, 100, 10, water_button_3, water_timer_3, water_label_3, "Hire Amphibious Gremlins", list.RES.SUBWATER)
+	water_passive_4 = resource_creator.new(1000, 10, 1, water_button_4, water_timer_4, water_label_4, "Construct Crystal Pulverizer", list.RES.WATER)
 	all_passives.append(water_passive_1)
 	all_passives.append(water_passive_2)
+	all_passives.append(water_passive_3)
+	all_passives.append(water_passive_4)
 	
 	gremlins.append(air_passive_1)
+	gremlins.append(air_passive_3)
 	gremlins.append(earth_passive_1)
+	gremlins.append(earth_passive_3)
 	gremlins.append(fire_passive_1)
+	gremlins.append(fire_passive_3)
 	gremlins.append(water_passive_1)
+	gremlins.append(water_passive_3)
 	
 	# Initial button text setup
 	for x in all_passives:
@@ -169,6 +208,20 @@ func _on_earth_passive2_pressed():
 
 func _on_earth_passive2_timeout():
 	extraction(payout(earth_passive_2), list.RES.SUBEARTH)
+	
+func _on_earth_passive3_pressed():
+	buy_creator(earth_passive_3)
+	SoundPlayer.play_category(SoundPlayer.GREMLINSOUNDS)
+
+func _on_earth_passive3_timeout():
+	res.add_sub_element(payout(earth_passive_3), list.RES.SUBEARTH)
+
+func _on_earth_passive4_pressed():
+	buy_creator(earth_passive_4)
+	SoundPlayer.play_sound(SoundPlayer.CLICK2)
+
+func _on_earth_passive4_timeout():
+	extraction(payout(earth_passive_4), list.RES.SUBEARTH)
 
 # Fire
 
@@ -185,6 +238,20 @@ func _on_fire_passive2_pressed():
 
 func _on_fire_passive2_timeout():
 	extraction(payout(fire_passive_2), list.RES.SUBFIRE)
+	
+func _on_fire_passive3_pressed():
+	buy_creator(fire_passive_3)
+	SoundPlayer.play_category(SoundPlayer.GREMLINSOUNDS)
+
+func _on_fire_passive3_timeout():
+	res.add_sub_element(payout(fire_passive_3), list.RES.SUBFIRE)
+
+func _on_fire_passive4_pressed():
+	buy_creator(fire_passive_4)
+	SoundPlayer.play_sound(SoundPlayer.CLICK2)
+
+func _on_fire_passive4_timeout():
+	extraction(payout(fire_passive_4), list.RES.SUBFIRE)
 
 # Water
 
@@ -201,6 +268,20 @@ func _on_water_passive2_pressed():
 
 func _on_water_passive2_timeout():
 	extraction(payout(water_passive_2), list.RES.SUBWATER)
+
+func _on_water_passive3_pressed():
+	buy_creator(water_passive_3)
+	SoundPlayer.play_category(SoundPlayer.GREMLINSOUNDS)
+
+func _on_water_passive3_timeout():
+	res.add_sub_element(payout(water_passive_3), list.RES.SUBWATER)
+
+func _on_water_passive4_pressed():
+	buy_creator(water_passive_4)
+	SoundPlayer.play_sound(SoundPlayer.CLICK2)
+
+func _on_water_passive4_timeout():
+	extraction(payout(water_passive_4), list.RES.SUBWATER)
 
 # Helper functions
 
