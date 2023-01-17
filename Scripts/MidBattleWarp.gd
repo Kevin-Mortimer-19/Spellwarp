b3dcaa04fcdf17a9090cdebcee7893b0a720cca9
Kevin-Mortimer-19 <70980411+Kevin-Mortimer-19@users.noreply.github.com>
2023-01-16 22:00:33 -0500

    boss fight warp resource wipe

--- Scripts/MidBattleWarp.gd
+++ Scripts/MidBattleWarp.gd
@@ -1,4 +1,9 @@
 extends Button
 
 func _on_warp_pressed():
+	StoredEnergy.air = 0
+	StoredEnergy.earth = 0
+	StoredEnergy.fire = 0
+	StoredEnergy.water = 0
+	StoredEnergy.light = 0
 	get_tree().change_scene("res://Scenes/Dimension.tscn")
